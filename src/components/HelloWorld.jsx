65bfafe6b7e2d94687d9049dd84dcdff5837b64e
santiago tovar <santiagoriscanevo@outlook.com>
2020-09-09 16:14:27 -0500

    entorno terminado

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import "../assets/style/App.scss"
 
-const HelloWorld = () => {
+const HelloWorld = () => (
     <h1>Hola mundo</h1>
-}
+)
 
 export default HelloWorld;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    // aca vamos a configurar cada uno de los elementos que necesitamos
+    //partiendo por la entrada referenciando a nuestro archivo principal
+    entry: './src/index.js',
+    //output donde vamos a guardar nuestros archivos resultantes cuando hagamos la complilacion
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "bundle.js"
+    }, 
+    resolve: { // resolver las extenciones que vamos a utilizar
+        extensions: ['.js','.jsx']
+    },
+    module: {
+        rules:[
+        {
+            test: /\.(js|jsx)$/, // regla principal identificar los archivos
+            exclude: /node_modules/, //exclusion ed la carpeta node_modules
+            use:{
+                loader:"babel-loader"
+            }
+        },
+        {
+            test:/\.html$/, // nos va a permitir trabajar con los archivos de HTML
+            use:[
+                {
+                    loader: 'html-loader'
+                }
+            ]
+        },
+        {
+            test: /\.(s*)css$/,
+            use:[
+                {
+                    loader:MiniCssExtractPlugin.loader
+                },
+                "css-loader",
+                "sass-loader"
+            ]
+        }           
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filname:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            //el primero es como voy a llamar el archivo resultante
+            filename:'assets/[name].css'
+        })
+    ]
+}
